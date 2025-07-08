9926474879a8ae54883b41f6cc1e863f989d8fc1
YacineG03 <132847649+YacineG03@users.noreply.github.com>
2025-07-08 21:19:43 +0000

    Dockerfile Backend

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -6,8 +6,6 @@ SECRET_PATH="/etc/secrets/chatbot-tournify-sxev-9978e5cfb5ad.json"
 echo "🔍 Checking if secret file exists at $SECRET_PATH"
 if [ -f "$SECRET_PATH" ]; then
     echo "📄 Exists"
-    echo "🔐 Changing permissions to be readable"
-    chmod 644 "$SECRET_PATH"
 else
     echo "⚠️ Secret file not found at $SECRET_PATH"
 fi
